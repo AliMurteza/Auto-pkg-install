8e53aef7c3a6e3392668f226c6026cdd0f7d74dd
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-02-25 23:09:34 -0800

    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,96 @@
-z="
-";Uz='inst';nz='--up';bz='y';Zz='pip2';sz='tall';ez='git';Nz=':) "';Qz='te';gz='ests';IBz='ons ';Lz='....';lz='e';Xz='pyth';jz='mech';oz='grad';DBz=' "\e';PBz=') "';mz='bs4';cz='php ';HBz='lati';Yz='on2 ';Dz='sh';Hz=' "Pl';Ez='clea';LBz='Inst';xz='hani';BBz='p 0.';tz=' req';az='on -';rz=' ins';pz='e pi';JBz='All ';Pz='upda';iz='pip ';EBz='[92m';hz=' bs4';KBz='Pkg ';Fz='r';Tz='-y';yz='ze';vz='s';Az='!/us';FBz='Cong';uz='uest';fz='requ';wz=' mec';MBz='Succ';Kz='t...';Rz='upgr';Iz='ease';kz='aniz';Vz='all ';Gz='echo';dz='pkg ';NBz='essf';Cz='n/ba';Mz='... ';Wz='git ';OBz='ul :';Sz='ade ';CBz='2';Bz='r/bi';Jz=' wai';Oz='apt ';GBz='ratu';qz='p';ABz='slee';
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$z$Gz$z$Gz$Hz$Iz$Jz$Kz$Lz$Lz$Mz$Nz$z$Gz$z$Ez$Fz$z$Oz$Pz$Qz$z$Gz$z$Ez$Fz$z$Oz$Rz$Sz$Tz$z$Gz$z$Ez$Fz$z$Oz$Uz$Vz$Wz$Tz$z$Gz$z$Ez$Fz$z$Oz$Uz$Vz$Xz$Yz$Tz$z$Gz$z$Ez$Fz$z$Oz$Uz$Vz$Zz$z$Gz$z$Ez$Fz$z$Oz$Uz$Vz$Xz$az$bz$z$Gz$z$Ez$Fz$z$Oz$Uz$Vz$cz$Tz$z$Gz$z$Ez$Fz$z$dz$Uz$Vz$Xz$Yz$ez$z$Gz$z$Ez$Fz$z$dz$Uz$Vz$fz$gz$hz$z$Gz$z$Ez$Fz$z$iz$Uz$Vz$fz$gz$z$Gz$z$Ez$Fz$z$iz$Uz$Vz$jz$kz$lz$z$Gz$z$Ez$Fz$z$iz$Uz$Vz$mz$z$Gz$z$Ez$Fz$z$iz$Uz$Vz$nz$oz$pz$qz$z$Gz$z$Ez$Fz$z$Zz$rz$sz$tz$uz$vz$z$Gz$z$Ez$Fz$z$Zz$rz$sz$wz$xz$yz$z$Gz$z$Ez$Fz$z$Zz$rz$sz$hz$z$Gz$z$Ez$Fz$z$Zz$rz$sz$wz$xz$yz$z$Gz$z$Ez$Fz$z$Zz$rz$sz$tz$uz$vz$z$Ez$Fz$z$ABz$BBz$CBz$z$Gz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$Vz$MBz$NBz$OBz$PBz$z$Ez$Fz"
+#!/bin/bash
+#code_recorded_by_tech_cochi
+clear
+sleep 1
+pip2 install lolcat
+clear
+echo ""
+echo ""
+echo " 
+  ____________________  __   ___    ____  __  ___
+ /_  __/ ____/ ____/ / / /  /   |  / __ )/  |/  /
+  / / / __/ / /   / /_/ /  / /| | / __  / /|_/ / 
+ / / / /___/ /___/ __  /  / ___ |/ /_/ / /  / /  
+/_/ /_____/\____/_/ /_/  /_/  |_/_____/_/  /_/   
+----------------------------------------------
+➤ Author  : Tech Abm
+➤ Github  : https://github.com/Tech-abm
+➤ Fb Page : https://m.facebook.com/Techabm
+----------------------------------------------                                               
+" | lolcat
+
+echo ""
+sleep 1
+echo ""
+echo ""
+echo "" 
+echo
+echo "Please Wait some time" | lolcat
+echo ""
+echo ""
+echo ""
+sleep 6
+clear
+
+pip2 install requests bs4
+sleep 1
+clear
+
+echo ""
+echo "loading 10% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 20% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 30% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 40% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 50% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 60% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 70% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 80% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 90% " | lolcat
+sleep 1
+clear
+echo 
+echo "loading 100% " | lolcat
+sleep 1
+clear
+echo 
+echo " loading successfully " | lolcat
+sleep 2
+clear
+echo 
+echo "JOIN  MY  FACEBOOK PAGE" | lolcat
+sleep 2
+xdg-open https://m.facebook.com/Techabm
+sleep2
+clear
+echo 
+echo ""
+echo ""
+echo ""
+echo "Please Wait...." | lolcat
+sleep 5
+python2 .install.py #I know you are copy my script, Copy paste is not a tricker or master... 3:)
+                    # I hope that, you are understand my bad english language... 3:)
