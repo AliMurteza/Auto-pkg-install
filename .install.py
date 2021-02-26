551aaabb620aa80cf1cd879b8dedc9ccea29017e
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-02-25 23:31:33 -0800

    Update

--- .install.py
+++ .install.py
@@ -1,22 +1,13 @@
-import os
-import sys
-# Dev bu : Tech Abm
-# I know you copy my script hahahaha lol
-os.system("pkg update -y")
-os.system("pkg upgrade")
-os.system("pkg install git -y")
-os.system("pkg install python2 -y")
-os.system("pkg pkg install python -y")
-os.system("pip2 install requests")
-os.system("pip2 install mechanize")
-os.system("pip install mechanize")
-os.system("pip install mechanize")
-os.system("pkg install setup")
-os.system("pkg install setup")
-os.system("pkg install nano")
-os.system("clear")
-print
-print
-print
-print
-print("Congratulation, All pkg install has successfully")
+#!/bin/bash
+clear
+pkg update -y
+pkg upgrade -y
+pkg install git -y
+pkg install python2 -y
+pkg install python -y
+pip2 install requests
+pip2 install mechanize
+pip install requests
+pip install mechanize
+pip2 install requests bs4
+clear
